8a0a27f9ed41c25b04da3d56f2d49ba578ef577b
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-06-07 12:14:35 +0200

    drugie zadanie

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,3 +1,20 @@
 public class StringCalculator   {
 
+    public int add(String numbers) {
+
+        if (numbers.length() == 0) {
+            return 0;
+        }
+
+        int result = 0;
+
+        String[] strings = numbers.split(",");
+
+        for (String s : strings) {
+            result += Integer.parseInt(s);
+        }
+
+        return result;
+    }
 }
+
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,14 +7,22 @@ public class StringCalculatorTest {
     private StringCalculator stringCalculator = new StringCalculator();
 
     @BeforeEach
-    public void beforeEach(){
-        System.out.println("Nowy obiek");
-        stringCalculator = new StringCalculator;
+    public void beforeEach() {
+        stringCalculator = new StringCalculator();
     }
 
     @Test
-    public void addTest(){
-        assertEquals();
+    public void addTest() {
+        assertEquals(0, stringCalculator.add(""));
+        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(3, stringCalculator.add("1,2"));
     }
 
-}
+
+    @Test
+    public void manyNumbersAddTest() {
+        assertEquals(6, stringCalculator.add("1,2,3"));
+        }
+    }
+
+
