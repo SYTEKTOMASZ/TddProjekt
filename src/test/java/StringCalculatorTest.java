3f95466a1837e04236402d1d8c63ca3d21822eef
SYTEKTOMASZ <59762629+SYTEKTOMASZ@users.noreply.github.com>
2020-06-07 14:06:58 +0200

    6 zadanie

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,5 @@
 import javafx.util.StringConverter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,13 +9,13 @@ public class StringCalculatorTest {
     private StringCalculator stringCalculator = new StringCalculator();
 
     @BeforeEach
-    public void beforeEach() {
+    public void beforeEach() throws Exception {
         stringCalculator = new StringCalculator();
     }
 
     @Test
     @DisplayName("Test dodawania")
-    public void addTest() {
+    public void addTest() throws Exception {
         assertEquals(0, stringCalculator.add(""));
         assertEquals(1, stringCalculator.add("1"));
         assertEquals(3, stringCalculator.add("1,2"));
@@ -23,20 +24,40 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("Wiele Liczb")
-    public void manyNumbersAddTest() {
+    public void manyNumbersAddTest() throws Exception {
         assertEquals(6, stringCalculator.add("1,2,3"));
         }
 
         @Test
         @DisplayName("test separatora")
-        public void separatorAddTest(){
+        public void separatorAddTest() throws Exception{
         assertEquals(6,stringCalculator.add("1\n2,3"));
         }
 
     @Test
     @DisplayName("Test generowanego separatora")
-    public void customLineDelimeterTest() {
+    public void customLineDelimeterTest() throws Exception {
         assertEquals(6, stringCalculator.add("//;\n1;2;3"));
+    }
+
+    @Test
+    @DisplayName("Wykrywanie liczn ujemnych")
+    public void negativeNumbersTest() throws Exception{
+        Exception exception = assertThrows(Exception.class, () -> {
+            stringCalculator.add("1,-2,-3");
+        });
+
+        System.out.println(exception.getLocalizedMessage());
+
+        assertTrue(exception.getLocalizedMessage().contains("-2"));
+        assertTrue(exception.getLocalizedMessage().contains("-3"));
+    }
+
+    @Test
+    @DisplayName("Pomijanie liczb większych niż 1000")
+    public void tooBigNumbers() throws Exception{
+        assertEquals(3,stringCalculator.add("2,1,1500,1001"));
+
     }
 
 
